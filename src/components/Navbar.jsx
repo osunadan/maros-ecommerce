97129229997885094c88d35fed22d1f1bcf70f8d
osunadan <osuna_dan@outlook.com>
2022-10-14 01:57:24 -0500

    Agregando Nav Bar, parte 1

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="containerNavbar">
+      <ul className="navBar">
+        <li>
+          <a href="#">Productos</a>
+        </li>
+        <li>
+          <a href="#">Nosotros</a>
+        </li>
+        <li>
+          <a href="#">Contacto</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
